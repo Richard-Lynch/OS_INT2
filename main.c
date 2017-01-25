4b1a27e740c19926f1339ff0dddb43831ddad5b8
Richard-Lynch <rich753@hotmail.com>
2017-01-25 23:10:38 +0000

    working

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h> 
 #define NUM_THREADS 6
 #define NUM_STEPS 50000.00
 
@@ -9,16 +10,19 @@
 #define B_location 20.12
 #define C 1
 #define X1 3
-#define X2 -1
+#define X2 -2
 #define X3 4
-#define X4 2
+#define X4 0
 #define X5 0
+#define reS 148207.00
 
 
 pthread_mutex_t lock;
 pthread_mutex_t lock1;
 long double sum = 0;
 long double STEP_SIZE;
+long double THRESHHOLD = 0.01;
+long double MAX_DEPTH = 4;
 //double function = 10.0;
 int* a;
 int* b;
@@ -34,88 +38,144 @@ long double function (long double x){
     return result;
 }
 
-void *intergrate(void *index){
-    //pthread_mutex_lock(&lock1);
+typedef struct {
+    //Or whatever information that you need
+    long double a;
+    long double b;
+    int level;
+} bounds;
 
-    int in = (int)index;
-    long double a = A_location + ((long double)(in)*STEP_SIZE);
-    long double b = a + STEP_SIZE;
+void *intergrate(void *bound){
+// create some local vars
+    long double* area = malloc(sizeof(long double));
+    //long double area = 0;
 
-    // if(in < 5){
-    //     printf("In: %d A: %Lf B: %Lf\n", in,a,b);
-    // }
-    // if(in > NUM_STEPS - 5){
-    //     printf("In: %d A: %Lf B: %Lf\n", in,a,b);
-    // }
+    long double a = ((bounds*)bound)->a;
+    long double b = ((bounds*)bound)->b;
 
-    //printf("Hello thread: %d : %Lf to %Lf\n", in, a, b);
-    long double area =   ( function(a) + ( function(b) - function(a) )*0.5  )* STEP_SIZE;
+    long double fa = fabsl(function(a));
+    long double fb = fabsl(function(b));
+    
+    int level = ((bounds*)bound)->level;
 
-    pthread_mutex_lock(&lock);
-    //printf("thread %d Adding Sum:", in);
-    sum = sum + area;
-    //printf(" %Lf\n", sum);
-    pthread_mutex_unlock(&lock);
+// if the value of the functions at the bounds is more than the threashold spilt it in half
+    if((fabsl(fb-fa)>THRESHHOLD) && (level < MAX_DEPTH)){
+        printf("Creating new threads\n");
 
-    //pthread_mutex_unlock(&lock1);    
-    pthread_exit(NULL);
+// create new bounds
+        bounds left, right;
+        left.a = a;
+        left.b = ((b-a))/2 + a;
+        left.level = ((bounds*)bound)->level + 1;
+        right.a = ((b-a))/2 + a;
+        right.b = b;
+        right.level = ((bounds*)bound)->level + 1;
+
+// create new threads
+        pthread_t left_t;
+        pthread_t right_t;
+        int rcl, rcr;
+
+// create threads recursivly
+        rcl = pthread_create(&left_t, NULL, intergrate, (void*)&left);
+        rcr = pthread_create(&right_t, NULL, intergrate, (void*)&right);      
+        if(rcl){
+            printf("ERROR - return code from left pthread_create: %d\n", rcl);
+            exit(-1);
+        }
+        if(rcr){
+            printf("ERROR - return code from right pthread_create: %d\n", rcr);
+            exit(-1);
+        }
+
+// join threads
+        void* left_a;
+        void* right_a;
+
+        rcl = pthread_join(left_t, &left_a);
+        double long left_area = *(long double*)left_a;
+        //free(left_a);
+        printf("left area: %Lf\n", left_area);
+
+        rcr = pthread_join(right_t, &right_a);
+        double long right_area = *(long double*)right_a;
+        //free(right_a);
+        printf("right area: %Lf\n", right_area);
+
+        if(rcl){
+            printf("ERROR - return code from left pthread_join: %d\n", rcl);
+            exit(-1);
+        }
+        if(rcr){
+            printf("ERROR - return code from right pthread_join: %d\n", rcr);
+            exit(-1);
+        }
+
+// calculate area
+        //if()
+        *area = *(long double*)left_a + *(long double*)right_a;
+
+        free(left_a);
+        free(right_a);
+
+    }else{
+// otherwise calc the area using trap rule
+        *area = (fa + (fabsl(fb - fa)) * 0.5 ) * (fabsl(b - a));
+    }
+//  output area
+    printf("Area: %Lf\n", *area); 
+    //return (void*)area;
+    pthread_exit(area);
 }
 
 int main(int argc, const char* argv[]){
     printf("hello\n");
-    STEP_SIZE = (B_location-A_location)/NUM_STEPS;
 
-    if (pthread_mutex_init(&lock, NULL) != 0)
-    {
-        printf("\nlock mutex init failed\n");
-        return 1;
-    }
-    //lock1 just ensures that a thread is allows to compeltly execute before another is created, so there is no print errors
-    if (pthread_mutex_init(&lock1, NULL) != 0)
-    {
-        printf("\nlock1 mutex init failed\n");
-        return 1;
-    }
+// create top thread vars
+    pthread_t top_t;
+    int rc;
+    bounds top_bounds;
+    top_bounds.a = A_location;
+    top_bounds.b = B_location;
+    top_bounds.level = 0;
 
-    pthread_t threads[(int)NUM_STEPS];
-    int rc, t;
-
-    printf("Creating %f threads.\n", NUM_STEPS);
+// start clock
     clock_t start = clock(), diff;
-    for(t=0; t<(int)NUM_STEPS; t++){
-        //pthread_mutex_lock(&lock1);
-        // printf("Creating thread %d.\n", t);
-        // printf(".");
-
-        rc = pthread_create(&threads[t], NULL, intergrate, (void*)t);        
-        if(rc){
-            printf("ERROR - return code from pthread_create: %d\n", rc);
-            exit(-1);
-        }
-        //pthread_mutex_unlock(&lock1);
+    printf("Creating top thread.\n");
+// create first thread
+    rc = pthread_create(&top_t, NULL, intergrate, (void*)&top_bounds);        
+    if(rc){
+        printf("ERROR - return code from top pthread_create: %d\n", rc);
+        exit(-1);
     }
-    printf("Finished Creating threads\n");
+    printf("Finished creating thread\n");
 
-    printf("Joining threads.\n");
-    for(t=0; t<(int)NUM_STEPS; t++){
-        //printf(".");
-        //printf("Joining thread %d.\n", t);
-        rc = pthread_join(threads[t], NULL);
-        if(rc){
-            printf("ERROR - return code from pthread_join: %d\n", rc);
-            exit(-1);
-        }
+// join the threads
+    void* total_area;
+    rc = pthread_join(top_t, &total_area);
+    if(rc){
+        printf("ERROR - return code from pthread_join: %d\n", rc);
+        exit(-1);
     }
-    diff = clock() - start;
     printf("Finished Joining threads\n");
+// stop the cock
+    diff = clock() - start;
 
-    printf("Result: %Lf\n", sum);
+// output result
+    long double result = *(long double*)total_area;
+    printf("Result: %Lf\n", result);
+    long double adiff = fabsl(result - reS);
+    printf("Differance: %Lf\n", adiff);
+    long double perc = (adiff/result)*100;
+    printf("Percentage: %Lf\n", perc);
 
+
+// output time
     int msec = diff * 1000 / CLOCKS_PER_SEC;
     printf("Time taken %d seconds %d milliseconds\n", msec/1000, msec%1000);
 
-    pthread_mutex_destroy(&lock);
-    pthread_mutex_destroy(&lock1);
+    // pthread_mutex_destroy(&lock);
+    // pthread_mutex_destroy(&lock1);
 
     return 0;
 }
